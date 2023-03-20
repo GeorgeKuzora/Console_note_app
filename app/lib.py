560ba4ab89e5f7df1611723cdfb3528600100f20
GeorgiyKuzora <rafale87@gmail.com>
2023-03-20 20:51:01 +0300

    refactor: reworked Note class + tests
    Made Note Class smaller, made methods more abstract.

--- app/lib.py
+++ app/lib.py
@@ -1,20 +1,9 @@
-import json
 from datetime import datetime
-
-# from tkinter.tix import ListNoteBook
-
-
-class UserInputPrompt:
-    @classmethod
-    def promptUserForString(cls, message):
-        user_string = input(message)
-        return user_string
 
 
 class Note:
     NOTE_ID_FORMAT = "%Y%m%d%H%M%S"
     NOTE_DT_FORMAT = "%Y%m%d%H%M%S%f"
-    NOTE_INPUT_STREAM_FORMAT = ("note_title", "note_body")
 
     def __init__(self) -> None:
         self.note_title = ""
@@ -23,47 +12,27 @@ class Note:
         self.note_last_modification_dt = datetime.now()
         self.note_id = self._noteIdFormat()
 
-    def _noteIdFormat(self):
+    def _noteIdFormat(self) -> str:
         _note_id = self.note_creation_dt.strftime(self.NOTE_ID_FORMAT)
         return _note_id
 
-    @classmethod
-    def createNote(cls, input_stream):
-        new_note = Note()
-        new_note.note_title = input_stream[cls.NOTE_INPUT_STREAM_FORMAT[0]]
-        new_note.note_body = input_stream[cls.NOTE_INPUT_STREAM_FORMAT[1]]
-        return new_note
+    def setTitleBody(self, title: str, body: str) -> None:
+        self.note_title = title
+        self.note_body = body
 
-    @classmethod
-    def createNoteFromJson(cls, note_data):
-        note = Note()
-        # note_data = json.loads(json_stream)
-        for key in note_data:
-            setattr(note, key, note_data[key])
-        note.note_creation_dt = datetime.strptime(
-            note.note_creation_dt, cls.NOTE_DT_FORMAT
-        )
-        note.note_last_modification_dt = datetime.strptime(
-            note.note_last_modification_dt, cls.NOTE_DT_FORMAT
-        )
-        return note
+    def setStorageData(self, storage_data: dict) -> None:
+        for key in storage_data:
+            if key == "note_creation_dt" or key == "note_last_modification_dt":
+                setattr(
+                    self, key, datetime.strptime(storage_data[key], self.NOTE_DT_FORMAT)
+                )
+            else:
+                setattr(self, key, storage_data[key])
 
-    def changeNote(self, input_stream):
-        if input_stream[self.NOTE_INPUT_STREAM_FORMAT[0]]:
-            self.note_title = input_stream[self.NOTE_INPUT_STREAM_FORMAT[0]]
-        if input_stream[self.NOTE_INPUT_STREAM_FORMAT[1]]:
-            self.note_body = input_stream[self.NOTE_INPUT_STREAM_FORMAT[1]]
-        self.note_last_modification_dt = datetime.now()
+    def getNoteId(self) -> str:
+        return self.note_id
 
-    def getFileSystemStream(self):
-        return self._convertToJson(), self._noteIdFormat()
-
-    def _convertToJson(self):
-        attr_dict = self._convertToDict()
-        attr_json = json.dumps(attr_dict)
-        return attr_json
-
-    def _convertToDict(self):
+    def getNoteData(self) -> dict:
         note_as_dict = {}
         note_as_dict.update(
             {
@@ -78,6 +47,6 @@ class Note:
         )
         return note_as_dict
 
-    def _formatDateTimeToString(self, _note_date_time):
+    def _formatDateTimeToString(self, _note_date_time: datetime) -> str:
         _note_date_time_string = datetime.strftime(_note_date_time, self.NOTE_DT_FORMAT)
         return _note_date_time_string
--- app/tests/test_lib.py
+++ app/tests/test_lib.py
@@ -1,91 +1,82 @@
 import unittest
 from datetime import datetime
-from lib import *
+from lib import Note
+
+
+class MockNote(Note):
+    def __init__(self) -> None:
+        self.note_title = "mock title"
+        self.note_body = "mock body"
+        self.note_creation_dt = datetime(2023, 1, 1, 0, 0, 0, 0)
+        self.note_last_modification_dt = datetime(2023, 1, 1, 0, 0, 0, 0)
+        self.note_id = "20230101000000"
+
+    def randomizeData(self) -> None:
+        self.note_title = "this is random title"
+        self.note_body = "this is random body"
+        self.note_creation_dt = datetime(2020, 6, 12, 13, 56, 30, 45632)
+        self.note_last_modification_dt = datetime(2021, 11, 20, 8, 28, 45, 74327)
+        self.note_id = "20200612135630"
 
 
 class TestNote(unittest.TestCase):
-    def setUp(self):
-        self.test_datetime = datetime(2023, 1, 1, 1, 1, 1, 1)
-        self.test_note = Note()
-        self.test_id_string = self.test_datetime.strftime(self.test_note.NOTE_ID_FORMAT)
-        self.test_datetime_string = self.test_datetime.strftime(
-            self.test_note.NOTE_DT_FORMAT
-        )
-        self.test_note.note_id = self.test_id_string
-        self.test_note.note_creation_dt = self.test_datetime
-        self.test_note.note_last_modification_dt = self.test_datetime
-        self.test_note.note_title = "test title"
-        self.test_note.note_body = "test body"
-        self.test_input_stream = {
-            Note.NOTE_INPUT_STREAM_FORMAT[0]: "test title",
-            Note.NOTE_INPUT_STREAM_FORMAT[1]: "test body",
-        }
-        self.test_change_stream = {
-            Note.NOTE_INPUT_STREAM_FORMAT[0]: "changed title",
-            Note.NOTE_INPUT_STREAM_FORMAT[1]: "changed body",
-        }
-        self.test_json = '{"note_title": "test title", "note_body": "test body", "note_id": "20230313194644", "note_creation_dt": "20230313194644353411", "note_last_modification_dt": "20230313194644353430"}'
-        self.test_dict = {
+    def setUp(self) -> None:
+        self.test_data = {
             "note_title": "test title",
             "note_body": "test body",
-            "note_id": "20230313194644",
-            "note_creation_dt": "20230313194644353411",
-            "note_last_modification_dt": "20230313194644353430",
+            "note_id": "20230101000000",
+            "note_creation_dt": "20230101000000000000",
+            "note_last_modification_dt": "20230101000000000000",
         }
 
-    def testNoteIdFormat(self):
-        formated_note_id = self.test_note._noteIdFormat()
-        self.assertEqual(formated_note_id, self.test_note.note_id)
+    def useTestData(self, mock_note: MockNote) -> None:
+        for key in self.test_data:
+            if key == "note_creation_dt" or key == "note_last_modification_dt":
+                setattr(
+                    mock_note,
+                    key,
+                    datetime.strptime(self.test_data[key], mock_note.NOTE_DT_FORMAT),
+                )
+            else:
+                setattr(mock_note, key, self.test_data[key])
 
-    def testFormatDateTimeToString(self):
-        formated_note_dt = self.test_note._formatDateTimeToString(
-            self.test_note.note_creation_dt
-        )
-        self.assertEqual(formated_note_dt, self.test_datetime_string)
+    def testNoteIdFormat(self) -> None:
+        mock_note = MockNote()
+        formated_note_id = mock_note._noteIdFormat()
+        self.assertEqual(formated_note_id, self.test_data["note_id"])
 
-    def testConvertToDict(self):
-        object_attr = self.test_note._convertToDict()
-        self.assertEqual(type(object_attr), type({"thisis": "dict"}))
+    def testSetTitleBody(self) -> None:
+        mock_note = MockNote()
+        mock_note.setTitleBody(
+            self.test_data["note_title"], self.test_data["note_body"]
+        )
+        self.assertEqual(mock_note.note_title, self.test_data["note_title"])
+        self.assertEqual(mock_note.note_body, self.test_data["note_body"])
 
-    def testGetFileSystemStream(self):
-        object_attr = self.test_note.getFileSystemStream()
-        self.assertEqual(type(object_attr[0]), type("string"))
-        self.assertEqual(type(object_attr[1]), type("string"))
+    def testSetStorageData(self) -> None:
+        mock_note = MockNote()
+        mock_note.randomizeData()
+        mock_note.setStorageData(self.test_data)
+        for key in self.test_data:
+            if key == "note_creation_dt" or key == "note_last_modification_dt":
+                value = datetime.strftime(
+                    getattr(mock_note, key), mock_note.NOTE_DT_FORMAT
+                )
+            else:
+                value = getattr(mock_note, key)
+            self.assertEqual(value, self.test_data[key])
 
-    def testCreateNote(self):
-        time_before_note_created = datetime.now()
-        user_test_note = Note.createNote(self.test_input_stream)
-        time_after_note_created = datetime.now()
-        id_from_note_cd = user_test_note.note_creation_dt.strftime(
-            user_test_note.NOTE_ID_FORMAT
-        )
-        self.assertEqual(user_test_note.note_title, "test title")
-        self.assertEqual(user_test_note.note_body, "test body")
-        self.assertTrue(
-            user_test_note.note_creation_dt >= time_before_note_created
-            and user_test_note.note_creation_dt <= time_after_note_created
-        )
-        self.assertTrue(
-            user_test_note.note_last_modification_dt >= time_before_note_created
-            and user_test_note.note_last_modification_dt <= time_after_note_created
-        )
-        self.assertTrue(user_test_note.note_id == id_from_note_cd)
-        self.assertEqual(type(user_test_note.json_data), type("string"))
+    def testGetNoteId(self) -> None:
+        mock_note = MockNote()
+        self.assertEqual(mock_note.getNoteId(), self.test_data["note_id"])
 
-    def testCreateNoteFromJson(self):
-        note = Note.createNoteFromJson(self.test_dict)
-        for key in self.test_dict:
-            self.assertEqual(getattr(note, key, None), self.test_dict[key])
+    def testFormatDateTimeToString(self) -> None:
+        mock_note = MockNote()
+        dt_str = mock_note._formatDateTimeToString(mock_note.note_creation_dt)
+        self.assertEqual(dt_str, self.test_data["note_creation_dt"])
 
-    def testChangeNote(self):
-        time_before_note_created = datetime.now()
-        self.test_note.changeNote(self.test_change_stream)
-        time_after_note_created = datetime.now()
-        self.assertEqual(
-            self.test_note.note_title, self.test_change_stream["note_title"]
-        )
-        self.assertEqual(self.test_note.note_body, self.test_change_stream["note_body"])
-        self.assertTrue(
-            self.test_note.note_last_modification_dt >= time_before_note_created
-            and self.test_note.note_last_modification_dt <= time_after_note_created
-        )
+    def testGetNoteData(self) -> None:
+        mock_note = MockNote()
+        self.useTestData(mock_note)
+        data = mock_note.getNoteData()
+        self.assertEqual(data, self.test_data)
